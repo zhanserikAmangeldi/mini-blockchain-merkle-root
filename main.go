b3d0ec25b064a6e05ebc82f10f3dfefedd2c7f30
zham0625 <zhanserik.amangeldi@netcracker.com>
2025-09-14 18:52:17 +0500

    add memorypool

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 type Transaction struct {
 	Hash      string
 	Amount    float64
@@ -19,6 +21,38 @@ type Block struct {
 
 type Blockchain struct {
 	Blocks []Block
+}
+
+type MemPool struct {
+	Transactions []Transaction
+	TimeToLive   int64
+}
+
+func (mempool *MemPool) AddTransaction(tx Transaction) {
+	mempool.Transactions = append(mempool.Transactions, tx)
+}
+
+func (mempool *MemPool) CleanUp() {
+	now := time.Now().Unix()
+	newTxs := []Transaction{}
+	for _, tx := range mempool.Transactions {
+		if now-tx.Timestamp <= mempool.TimeToLive {
+			newTxs = append(newTxs, tx)
+		}
+	}
+
+	mempool.Transactions = newTxs
+}
+
+func (mempool *MemPool) GetTransactions(limit int) []Transaction {
+	mempool.CleanUp()
+	if len(mempool.Transactions) < limit {
+		limit = len(mempool.Transactions)
+	}
+	txs := mempool.Transactions[:limit]
+	mempool.Transactions = mempool.Transactions[limit:]
+
+	return txs
 }
 
 func (blockchain *Blockchain) AddBlock(newBlock Block) {
