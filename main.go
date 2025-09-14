e1f4c46e973b90f21e3297f50d410d312736acdd
zham0625 <zhanserik.amangeldi@netcracker.com>
2025-09-14 19:26:12 +0500

    add full compleated impl of blockchain

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type Transaction struct {
 	Hash      string
@@ -10,6 +14,26 @@ type Transaction struct {
 	Timestamp int64
 }
 
+func (tx *Transaction) IsValid() bool {
+	return tx.Amount > 0 && tx.From != "" && tx.To != ""
+}
+
+func (tx *Transaction) ComputeHash() string {
+	hash := tx.From + tx.To + strconv.FormatFloat(tx.Amount, 'f', 6, 64) + strconv.FormatInt(tx.Timestamp, 10)
+	return hash
+}
+
+func NewTransaction(from, to string, amount float64) *Transaction {
+	tx := &Transaction{
+		From:      from,
+		To:        to,
+		Amount:    amount,
+		Timestamp: time.Now().Unix(),
+	}
+	tx.Hash = tx.ComputeHash()
+	return tx
+}
+
 type Block struct {
 	BlockHeight  int
 	Hash         string
@@ -17,6 +41,41 @@ type Block struct {
 	Timestamp    int64
 	Transactions []Transaction
 	MerleRoot    string
+}
+
+func (block *Block) IsValid() bool {
+	return block.BlockHeight >= 0 && block.Hash != "" && block.PrevHash != "" && len(block.Transactions) > 0
+}
+
+func (block *Block) ComputeHash() string {
+	hash := strconv.Itoa(int(block.Timestamp))
+	return hash
+}
+
+func (block *Block) AddTransaction(tx Transaction) {
+	if !tx.IsValid() {
+		return
+	}
+
+	block.Transactions = append(block.Transactions, tx)
+}
+
+func NewBlock(prevBlock *Block, transactions []Transaction) *Block {
+	height := 0
+	prevHash := "0000000000000000000000000000000000000000000000000000000000000000000"
+	if prevBlock != nil {
+		height = prevBlock.BlockHeight + 1
+		prevHash = prevBlock.Hash
+	}
+
+	newBlock := &Block{
+		BlockHeight:  height,
+		PrevHash:     prevHash,
+		Timestamp:    time.Now().Unix(),
+		Transactions: transactions,
+	}
+	newBlock.Hash = newBlock.ComputeHash()
+	return newBlock
 }
 
 type Blockchain struct {
@@ -29,6 +88,9 @@ type MemPool struct {
 }
 
 func (mempool *MemPool) AddTransaction(tx Transaction) {
+	if !tx.IsValid() {
+		return
+	}
 	mempool.Transactions = append(mempool.Transactions, tx)
 }
 
@@ -56,8 +118,34 @@ func (mempool *MemPool) GetTransactions(limit int) []Transaction {
 }
 
 func (blockchain *Blockchain) AddBlock(newBlock Block) {
+	if !newBlock.IsValid() {
+		return
+	}
 	blockchain.Blocks = append(blockchain.Blocks, newBlock)
 }
 
 func main() {
+	blockchain := &Blockchain{}
+	mempool := &MemPool{TimeToLive: 300}
+
+	tx1 := NewTransaction("Aral", "Zhanserik", 100)
+	mempool.AddTransaction(*tx1)
+	tx2 := NewTransaction("Danyal", "Ilyas", 50)
+	mempool.AddTransaction(*tx2)
+	tx3 := NewTransaction("Zhanserik", "Danyal", 30)
+	mempool.AddTransaction(*tx3)
+	fmt.Println("Mempool before creating block:", mempool.Transactions)
+
+	genesisBlock := NewBlock(nil, mempool.GetTransactions(3))
+
+	fmt.Println(genesisBlock)
+	blockchain.AddBlock(*genesisBlock)
+
+	fmt.Println("Blockchain:", blockchain)
+	tx4 := NewTransaction("Aral", "Ilyas", 70)
+	mempool.AddTransaction(*tx4)
+
+	blockSecond := NewBlock(genesisBlock, mempool.GetTransactions(3))
+	blockchain.AddBlock(*blockSecond)
+	fmt.Println(blockchain)
 }
